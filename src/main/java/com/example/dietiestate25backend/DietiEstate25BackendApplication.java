c5433370037a0233ad9d3b25607e1f8e0ad98dee
taekwondodev <taekwondodev@outlook.it>
2024-12-24 12:24:42 +0100

    Creating the new repo

---
+++ src/main/java/com/example/dietiestate25backend/DietiEstate25BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dietiestate25backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietiEstate25BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietiEstate25BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dietiestate25backend/DietiEstate25BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dietiestate25backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietiEstate25BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
