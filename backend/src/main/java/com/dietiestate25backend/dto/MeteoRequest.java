bdad0c6d367b1e52e334d2f81701ed5bd68da33c
antoniodim <antoniodimaso.adm1511@gmail.com>
2025-01-21 18:11:21 +0100

    Aggiornato il DTO MeteoRequest con i get e i set, necessari a MeteoController per spacchettarlo

--- backend/src/main/java/com/dietiestate25backend/dto/MeteoRequest.java
+++ backend/src/main/java/com/dietiestate25backend/dto/MeteoRequest.java
@@ -3,4 +3,20 @@ package main.java.com.dietiestate25backend.dto;
 public class MeteoRequest {
     private String city;
     private String date;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
